9266f2f99b3f9c7116f4999bd69c04a7e27c199b
vnhai <haingocvu@hotmail.com>
2018-06-29 09:46:38 +0700

    use react with webpack:

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+let path = require('path');
+
+const config = {
+    mode: 'development',
+    entry: {
+        bundle: './src/index.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+}
+
+module.exports = config;
